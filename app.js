0e5eb98415ef2866b64ba22c8abc635dead4821d
shubham8596 <spj.8596@gmail.com>
2020-09-04 14:17:58 +0530

    Video Chat App

---
+++ app.js
@@ -0,0 +1,20 @@
+let express = require( 'express' );
+let app = express();
+let server = require( 'http' ).Server( app );
+let io = require( 'socket.io' )( server );
+let stream = require( './stream' );
+let path = require( 'path' );
+
+
+app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+
+app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/index.html' );
+} );
+
+io.of( '/stream' ).on( 'connection', stream );
+
+server.listen( 3000,() =>  
+	console.log("Server running on port 3000.....")
+);
+
