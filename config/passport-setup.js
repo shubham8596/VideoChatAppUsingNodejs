514ea467d4c0fe57e944a7487e53077c7b9814b3
shubham8596 <spj.8596@gmail.com>
2020-09-17 19:21:07 +0530

    signup done

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -12,8 +12,8 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID:"972605042244-ag8lb3a3m7qtccljcqqtbpmfciim1mqd.apps.googleusercontent.com",
-    clientSecret:"R1nybV--aWA3IFLi4cMl91oZ",
+    clientID:GOOGLE_CLIENT_ID,
+    clientSecret:GOOGLE_CLIENT_SECRET,
     callbackURL:"http://localhost:3000/signup"
   },
   function(request, accessToken, refreshToken, profile, done) {
